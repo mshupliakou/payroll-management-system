cba1aa871130c26b60e517393df512ee4af1851e
mshupliakou <mikhail.shupliakou@gmail.com>
2025-12-04 23:46:29 +0100

    Created position dto

---
+++ src/main/java/com/project_agh/payrollmanagementsystem/dtos/PositionDto.java
@@ -0,0 +1,10 @@
+package com.project_agh.payrollmanagementsystem.dtos;
+
+import lombok.Data;
+
+@Data
+public class PositionDto {
+
+    private String position_name;
+    private String position_desc;
+}
