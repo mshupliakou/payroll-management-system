28439754ccb1ace37a5c902f2005406025c7bc94
mshupliakou <mikhail.shupliakou@gmail.com>
2025-12-04 23:24:43 +0100

    Just Department DTO

---
+++ src/main/java/com/project_agh/payrollmanagementsystem/dtos/CreateDepartmentDto.java
@@ -0,0 +1,26 @@
+package com.project_agh.payrollmanagementsystem.dtos;
+
+import lombok.Data;
+
+/**
+ * DTO used for creating a new department in the system.
+ * <p>
+ * Contains basic information required to define a department:
+ * its name and a textual description.
+ * </p>
+ */
+@Data
+public class CreateDepartmentDto {
+
+    /**
+     * The name of the department.
+     * Should be unique and clearly identify the department.
+     */
+    private String department_name;
+
+    /**
+     * A short textual description of the department.
+     * Used to give more context about the department's purpose or responsibilities.
+     */
+    private String department_desc;
+}
