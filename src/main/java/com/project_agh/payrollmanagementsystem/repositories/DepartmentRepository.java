48bc887d46821ff3cf7bd12a60311441585ba957
mshupliakou <mikhail.shupliakou@gmail.com>
2025-12-04 23:19:17 +0100

    Added to model create department form

--- src/main/java/com/project_agh/payrollmanagementsystem/repositories/DepartmentRepository.java
+++ src/main/java/com/project_agh/payrollmanagementsystem/repositories/DepartmentRepository.java
@@ -1,18 +1,15 @@
 package com.project_agh.payrollmanagementsystem.repositories;
-
 import com.project_agh.payrollmanagementsystem.entities.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-/**
- * Repository interface for managing {@link Department} entities.
- * <p>
- * This interface extends Spring Data JPA's {@link JpaRepository}, providing
- * standard CRUD (Create, Read, Update, Delete) operations and basic querying
- * capabilities for the Department entity, using {@code Long} as the type
- * of the primary key (ID).
- */
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    // Custom query methods can be defined here, following Spring Data JPA naming conventions.
-    // Example:
-    // Optional<Department> findByName(String name);
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface DepartmentRepository{
+    List<Department> findAll();
+
+    void createDepartment(String department_name, String department_desc);
 }
+
