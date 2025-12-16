3b8dfc6b7348c335d8980ed0a95abba824822b22
mshupliakou <mikhail.shupliakou@gmail.com>
2025-12-16 17:55:50 +0100

    PaymentTypeDto was created

---
+++ src/main/java/com/project_agh/payrollmanagementsystem/dtos/PaymentTypeDto.java
@@ -0,0 +1,11 @@
+package com.project_agh.payrollmanagementsystem.dtos;
+
+import lombok.Data;
+
+@Data
+public class PaymentTypeDto {
+
+    private Long id;
+    private String name;
+    private String description;
+}
