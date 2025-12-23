e1453f77eb4466837772c4e3599780e8ff002c02
mshupliakou <mikhail.shupliakou@gmail.com>
2025-12-23 20:26:50 +0100

    Payment entity was created

--- src/main/java/com/project_agh/payrollmanagementsystem/entities/Payment.java
+++ src/main/java/com/project_agh/payrollmanagementsystem/entities/Payment.java
@@ -1,4 +1,44 @@
 package com.project_agh.payrollmanagementsystem.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "historia_wyplat")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_wyplata")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pracownik", nullable = false)
+    private User user;
+
+    @Column(name = "data", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "wyplata")
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "id_status_wyplaty")
+    private PaymentStatus paymentStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "id_typ_wyplaty")
+    private PaymentType paymentType;
+
+    @Column(name = "opis")
+    private String description;
+
 }
